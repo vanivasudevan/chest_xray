b5d2f525d861c2f5dec3ca9857147b5088c15089
vanivasudevan <vasudevan.vani@gmail.com>
2024-05-03 11:48:11 +0530

    prediction done

--- pred_image.py
+++ pred_image.py
@@ -11,8 +11,13 @@ from keras.preprocessing import image
 from keras.applications.vgg16 import preprocess_input
 import numpy as np
 model=load_model('model_vgg16.h5')
-img = image.load_img('val/NORMAL/NORMAL2-IM-1427-0001.jpeg',target_size=(224,224))
+img = image.load_img('val/PNEUMONIA/person1946_bacteria_4874.jpeg',target_size=(224,224))
 x=image.img_to_array(img)
 x=np.expand_dims(x, axis=0)
 img_data = preprocess_input(x)
 classes = model.predict(img_data)
+
+if(classes[0,0]== 1):
+    print ("X-ray is Normal")
+else:
+    print ("X-ray is Abnormal and patient is affected by pneumonia")
